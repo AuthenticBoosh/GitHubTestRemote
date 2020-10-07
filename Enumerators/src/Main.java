2adab0eec17ef8c177891dd493dafca5adadbee3
Ethan <Ethan@10.0.0.94>
2020-10-07 16:40:23 -0600

    Trying to add a separate project to the repository (hope this works)

---
+++ Enumerators/src/Main.java
@@ -0,0 +1,19 @@
+
+enum Level {
+	LOW, MEDIUM, HIGH;
+}
+
+public class Main {
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Level l = Level.MEDIUM;
+		
+		System.out.println(l);
+		
+		
+	}
+
+}
