e206f4686b0d2d95c62459db00c7f661b035bb8d
Ethan <Ethan@10.0.0.94>
2020-10-07 16:22:50 -0600

    Added features showcasing inheritance.

--- GithubTest/src/Main.java
+++ GithubTest/src/Main.java
@@ -4,6 +4,10 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Mouse1 m = new Mouse1();
+		m.leftClick(); // Inheriting the Mouse.java feature of left click
+		m.rgb(); // Mouse1's already defined feature of changing colors.
+		
 	}
 
 }
